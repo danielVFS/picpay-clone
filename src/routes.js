1c2a333feb14a587f5b8eff9fb8fdd64628e7a48
danielVFS <danielv759153@gmail.com>
2020-08-27 15:24:56 -0300

    configuring tabBar navigation

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { AntDesign, Ionicons } from '@expo/vector-icons';
 
 import Home from './pages/Home';
 import Wallet from './pages/Wallet';
@@ -7,9 +8,47 @@ import Pay from './pages/Pay';
 
 const Tab = createBottomTabNavigator();
 
+const icons = {
+  Home: {
+    lib: AntDesign,
+    name: 'home',
+  },
+  Wallet: {
+    lib: AntDesign,
+    name: 'creditcard',
+  },
+  Pay: {
+    lib: AntDesign,
+    name: 'home',
+  },
+  Notifications: {
+    lib: Ionicons,
+    name: 'ios-notifications-outline',
+  },
+  Settings: {
+    lib: AntDesign,
+    name: 'setting',
+  },
+};
+
 export default function Routes() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          const { lib: Icon, name } = icons[route.name];    
+          return <Icon name={name} size={size} color={color}/>
+        },
+      })}
+      tabBarOptions={{
+        style: {
+          backgroundColor: '#131418',
+          borderTopColor: 'rgba(255, 255, 255, 0.2)',
+        },
+        activeTintColor: '#FFF',
+        inactiveTintColor: '#92929c',
+      }}
+    >
       <Tab.Screen 
         name="Home" 
         component={Home}

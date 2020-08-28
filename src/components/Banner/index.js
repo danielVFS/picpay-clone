bde86e5cd7722e30395d1c79fa14d9ea9841da79
danielVFS <danielv759153@gmail.com>
2020-08-28 19:54:20 -0300

    banner bottom component add to home

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
-import { Container} from './styles';
+import { Container, Details, Title, Description ,Image} from './styles';
+
+import img13 from '../../assets/13.png';
 
 export default function Banner() {
   return (
     <Container >
-     
+      <Details>
+        <Title>Cobre um amigo</Title>
+
+        <Description>Mantenha suas parcerias em dia, use o PicPay para fazer suas cobranças</Description>
+      </Details>
+      <Image source={img13}/>
     </Container>
   ); 
 }

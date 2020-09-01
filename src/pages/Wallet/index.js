73a56106a5efa658e6165f4e4779482f875269df
danielVFS <danielv759153@gmail.com>
2020-09-01 18:23:10 -0300

    wallet header finish

--- src/pages/Wallet/index.js
+++ src/pages/Wallet/index.js
@@ -1,8 +1,10 @@
 import React,{ useCallback } from 'react';
 import { useFocusEffect } from "@react-navigation/native";
 import { StatusBar, Platform } from 'react-native';
+import { Feather, MaterialCommunityIcons, FontAwesome } from '@expo/vector-icons';
 
-import { Wrapper, Header } from './styles';
+import { Wrapper, Header, HeaderContainer, Title, BalanceContainer, 
+  Value, Bold, EyeButton, Info, Actions, Action, ActionLabel } from './styles';
 
 export default function Wallet() {
   useFocusEffect(
@@ -16,6 +18,35 @@ export default function Wallet() {
    <Wrapper>
      <StatusBar barStyle="dark-content" backgroundColor="#52E78C"/>
      <Header colors={['#52e78c', '#1ab563']}>
+        <HeaderContainer>
+          <Title>Saldo do PicPay</Title>
+
+          <BalanceContainer>
+            <Value>
+              R$ <Bold>0,00</Bold>
+            </Value>
+
+            <EyeButton>
+              <Feather name="eye" size={28} color="#FFF"/>
+            </EyeButton>
+          </BalanceContainer>
+
+          <Info>
+            Seu saldo está rendendo 15$ do CDI
+          </Info>
+
+          <Actions>
+            <Action>
+              <MaterialCommunityIcons name="cash" size={30} color="#FFF"/>
+              <ActionLabel>Adicionar</ActionLabel>
+            </Action>
+            <Action>
+              <FontAwesome name="bank" size={20} color="#FFF"/>
+              <ActionLabel>Retirar</ActionLabel>
+            </Action>
+          </Actions>
+        </HeaderContainer>
+
 
      </Header>
    </Wrapper>
--- src/pages/Wallet/styles.js
+++ src/pages/Wallet/styles.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components/native';
 import { LinearGradient } from 'expo-linear-gradient';
 
-
 export const Wrapper = styled.View`
   background: #000;
   flex: 1;
@@ -9,4 +8,65 @@ export const Wrapper = styled.View`
 
 export const Header = styled(LinearGradient)`
   height: 300px;
+`;
+
+export const HeaderContainer = styled.SafeAreaView`
+  flex: 1;
+  justify-content: center;  
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  color: #FFF;
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+export const BalanceContainer = styled.View`
+  margin: 10px 0px;
+
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Value = styled.Text`
+  color: #FFF;
+  font-size: 38px;
+  font-weight: 200;
+`;
+
+export const Bold = styled.Text`
+  font-weight: bold;
+`;
+
+export const EyeButton = styled.TouchableOpacity`
+  margin-left: 10px;
+`;
+
+export const Info = styled.Text`
+  color: #FFF;
+  font-size: 14px;
+`;
+
+export const Actions = styled.View`
+  margin-top: 40px;
+  flex-direction: row;
+`;
+
+export const Action = styled.TouchableOpacity`
+  border: 1px solid rgba(255, 255, 255, 0.6);
+  width: 150px;
+  height: 45px;
+  border-radius: 25px;
+  margin: 0 10px;
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ActionLabel = styled.Text`
+  color: #FFF;
+  font-size: 16px;
+  margin-left: 10px;
 `;
